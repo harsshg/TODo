a817b911f3f7ab465a850de59de17ffdea5c574b
harsshg <harshgupta1.hp@gmail.com>
2025-07-24 12:05:39 +0530

    ui created

--- App.tsx
+++ App.tsx
@@ -1,28 +1,173 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import React from 'react';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const App = () => {
+  let styles = StyleSheet.create({
+    container: {
+      backgroundColor:"",
+      height: '100%',
+      width: '100%',
+      marginTop: 50,
+      padding: 10,
+      fontFamily: 'SFProDisplay-Regular',
+    },
+    viewbox: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 20,
+      height: 100,
+    },
+    TextStyle: {
+      
+      fontWeight: 'bold',
+      fontSize: 30,
+      color: 'black',
+    },
+    ADD: {
+      fontSize: 30,
+      fontWeight: '400',
+      borderWidth: 3,
+      color: '#006CFF',
+      paddingHorizontal: 12,
+      paddingVertical: 0,
+      borderColor: '#006CFF',
+      borderRadius: 50,
+    },
+    taskContainer: {
+      padding: 20,
+      display: 'flex',
+      flexDirection: 'row',
+      borderBottomWidth:1,
+      borderBottomColor: '#a6a6a6',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    tick:{
+      height:30,
+      width:30,
+      borderRadius: 50,
+      borderWidth: 2,
+      borderColor: '#006CFF',
+    },
+    texttodo:{
+      fontSize: 20,
+      fontWeight: '400',
+     
+    },
+    dot:{
+      height: 18,
+      width: 18,
+      borderRadius: 50,
+      backgroundColor: 'red',
+      marginLeft: 10,
+    },
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+    // Here View starts
+    AddTask:{
+      shadowOffset: { width: 0, height: 2 },
+      shadowColor : 'black',
+      
+      borderWidth: 1,
+      borderRadius: 20,
+      padding: 20,
+      borderColor: '#a6a6a6',
+      marginHorizontal: "auto",
+     marginTop: 50,
+     display: 'flex',
+     flexDirection: 'column',
+     gap: 10,
+    },
+     Addtasktext:{
+        fontSize: 30,
+        fontWeight: '700',
+        color: 'black',
+        
+      },
+      Addtaskmsgbx:{
+       height: 250,
+       width: 350,
+       borderColor: '#a6a6a6',
+       borderWidth: 1,
+       borderRadius: 20,
+      },
+      Addtaskcd:{
+       display: 'flex',
+       flexDirection: 'row',
+       justifyContent: 'space-between',
+       marginHorizontal: 10,
+      },
+      AddtaskDone:{
+        fontSize: 25,
+        
+        color: '#006CFF',
+        fontWeight: 800,
+        marginRight: 10,
+      },
+      AddtaskCreate:{
+        fontSize: 25,
+        fontWeight: '500',
+
+        color: '#006CFF',
+      }
+  });
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.viewbox}>
+        <Text style={styles.TextStyle}>Today</Text>
+        <Text style={styles.ADD}>+</Text>
+      </View>
+      
+      <View style = {[styles.taskContainer]}>
+       <View style={{flexDirection: 'row', alignItems: 'center',gap: 10}}>
+        <View style={styles.tick}></View>
+        <Text style={styles.texttodo}>Pay for rent</Text>
+       </View>
+       <View style={styles.dot}></View>
+      </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+      <View style = {[styles.taskContainer,{marginTop: 20}]}>
+       <View style={{flexDirection: 'row', alignItems: 'center',gap: 10}}>
+        <View style={styles.tick}></View>
+        <Text style={styles.texttodo}>Buy a milk</Text>
+       </View>
+       <View style={[styles.dot,{backgroundColor:"#B6F500"}]}></View>
+      </View>
+
+      <View style = {[styles.taskContainer,{marginTop: 20}]}>
+       <View style={{flexDirection: 'row', alignItems: 'center',gap: 10}}>
+        <View style={styles.tick}></View>
+        <Text style={styles.texttodo}>Don’t forget to pick up Mickael</Text>
+       </View>
+       <View style={[styles.dot,{backgroundColor:"#56DFCF"}]}></View>
+      </View>
+
+      <View style = {[styles.taskContainer,{marginTop: 20}]}>
+       <View style={{flexDirection: 'row', alignItems: 'center',gap: 10}}>
+        <View style={styles.tick}></View>
+        <Text style={styles.texttodo}>Buy a chocolate for Charlotte</Text>
+       </View>
+       <View style={[styles.dot,{backgroundColor:"#C68EFD"}]}></View>
+      </View>
+
+
+     {/* ADD TODO */}
+
+     <View style={styles.AddTask}>
+      <Text style={styles.Addtasktext}>Add Todo</Text>
+      <View style={styles.Addtaskmsgbx}></View>
+      <View style={styles.Addtaskcd}>
+        <Text style={styles.AddtaskCreate}>Cancel</Text>
+        <Text style={styles.AddtaskDone}>Done</Text>
+      </View>
+     </View>
+      
+
+
+    </SafeAreaView>
+  );
+};
 
 export default App;
